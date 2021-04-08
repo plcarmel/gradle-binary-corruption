0a5911be620c5aefa45896b8b5697214079d4d23
Pierre-Luc Carmel Biron <plbc@hotmail.com>
2021-04-08 09:57:12 -0400

    Remove arguments

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ./gradlew build
 echo "###### With Gradle:"
-gradle -q run --args "hello world" | md5sum
-gradle -q run --args "hello world" | wc -c
+gradle -q run | md5sum
+gradle -q run | wc -c
 cd ./build/classes/java/main || exit 1
 echo "###### Directly with the java command:"
-java net.plcarmel.gradlebinarycorruption.Main hello world | md5sum
-java net.plcarmel.gradlebinarycorruption.Main hello world | wc -c
+java net.plcarmel.gradlebinarycorruption.Main | md5sum
+java net.plcarmel.gradlebinarycorruption.Main | wc -c
